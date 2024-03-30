c02eee39ce732458a7a2ba3ed9929c2390de7a49
jiacwjcw <quota0619@gmail.com>
2024-03-30 23:12:47 +0800

    Initial Commit

---
+++ HelloUIView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloUIView
+//
+//  Created by Andrew Wu on 2024/3/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
